cdc6a845c338efc14163ff219411a99c094e87fb
Connor_Crump <ccrump@uoregon.edu>
2023-03-13 12:03:10 -0700

    revereted last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
+import styles from './index.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import {
